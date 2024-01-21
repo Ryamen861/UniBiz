cbd7b7f016095b513ff19abecc0f86eb5a2e82b0
Ryamen861 <ryanmoon2050@gmail.com>
2024-01-21 16:51:53 -0500

    impl padding

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+use core::num;
 use std::io;
 mod tools;
 fn main() {
 
     let name: String = get_name();
     let university: String = get_college();
-    println!("Hey, {}! You have recently been chosen to become the president of {} University", name, university);
+    let mut money: i32 = 20_000;
+    // generate students
+    let mut num_of_students: i32 = 0;
+    let mut avg_gpa: f32 = 3.2;
 
-    println!("                       UniBiz                         ");
-    let mut title: String = format!("{} of {}", &name, &university);
+    welcome(&name, &university, &money, &num_of_students, &avg_gpa);
+
+    let title: String = format!("Prez {} of {} University", &name, &university);
+    let row_one: String = format!("Funds: ${}", &money);
+    let row_two: String = format!("# of Students: {}", &num_of_students);
+    let row_three: String = format!("Avg GPA: {}", &avg_gpa);
+
+    println!("{}", tools::pad(title, false));
     
-    
-    println!("------------------------------------------------------");
-    println!("|                                                    |");
-    println!("|                                                    |");
-    println!("|                                                    |");
-    println!("------------------------------------------------------");
+    println!("----------------------------------------------------------------------------------------------");
+    // println!("|                                                                                            |");
+    // println!("|                                                                                            |");
+    // println!("|                                                                                            |");
+
+    println!("{}", tools::pad(row_one, true));
+    println!("{}", tools::pad(row_two, true));
+    println!("{}", tools::pad(row_three, true));
+    println!("----------------------------------------------------------------------------------------------");
 }
 
 fn get_name() -> String {
@@ -33,4 +46,13 @@ fn get_college() -> String {
     io::stdin().read_line(&mut university).expect("Error receiving college");
     university = university.trim().to_uppercase();
     return university
+}
+
+fn welcome(name: &String, uni: &String, money: &i32, stu: &i32, gpa: &f32) {
+    println!("Hey, {}! You have recently been chosen to become the president of {} University", name, uni);
+    println!("There are currently: ");
+    println!("  ${} in university funds", money);
+    println!("  {} students currently enrolled", stu);
+    println!("  They have an average gpa of {}", gpa);
+    println!("\n    We hope you will develop the university into a prominent, impactful community.")
 }
--- src/tools.rs
+++ src/tools.rs
@@ -1,10 +1,45 @@
-pub fn pad(mut s: String) -> &str {
+pub fn pad(mut s: String, bookend: bool) -> String {
     // should be 54 lines
-    let more_spaces: i32 = 54 - s.len() as i32 - 2;
+    let more_spaces: i32 = 94 - s.len() as i32;
+    let each_side: i32 = more_spaces / 2;
 
-    for i in 0..more_spaces {
-        s.push_str(" ");
+    let mut left_side: String = String::new();
+    let mut right_side: String = String::new();
+
+    if !bookend {
+        // add regular padding of spaces
+        for _ in 0..each_side {
+            left_side.push_str(" ");
+            right_side.push_str(" ");
+        }
+
+        // add extra on left if needed
+        if left_side.len() + s.len() + right_side.len() < 94 {
+            left_side.push_str(" ");
+        }    
+        
+    } else {
+        // right: pad spaces but leave room for bookend
+        for _ in 0..each_side - 1 {
+            right_side.push_str(" ");
+        }
+        // add right bookend
+        right_side.push_str("|");
+
+        // left: add bookend first, then add spaces
+        left_side.push_str("|");
+        for _ in 0..each_side {
+            left_side.push_str("|");
+        }
+
+        // add left extra if needed
+        if left_side.len() + s.len() + right_side.len() < 94  {
+            left_side.push_str(" ");
+        }
     }
+        
+    s.push_str(&right_side); // --- + ---
+    left_side.push_str(&s); // --- + (--- + --)
 
-    return " "
+    return left_side
 }
